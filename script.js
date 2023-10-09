1ed1759869f4341042bedd872ec7bec85c6a9b82
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-10-09 08:41:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-//your JS code here. If required.
+function addToCallStack(functionName) {
+            const callStackElement = document.getElementById('call-stack');
+            const div = document.createElement('div');
+            div.textContent = `Called: ${functionName}`;
+            callStackElement.appendChild(div);
+        }
+
+        function removeFromCallStack() {
+            const callStackElement = document.getElementById('call-stack');
+            if (callStackElement.lastChild) {
+                callStackElement.removeChild(callStackElement.lastChild);
+            }
+        }
+
+        function foo() {
+            addToCallStack('foo');
+            bar();
+            removeFromCallStack();
+        }
+
+        function bar() {
+            addToCallStack('bar');
+            baz();
+            removeFromCallStack();
+        }
+
+        function baz() {
+            addToCallStack('baz');
+            // Simulate some work
+            for (let i = 0; i < 1000000; i++) {
+                // Do nothing
+            }
+            removeFromCallStack();
+        }
+
+        function simulateCallStack() {
+            addToCallStack('simulateCallStack');
+            foo();
+            removeFromCallStack();
+        }
